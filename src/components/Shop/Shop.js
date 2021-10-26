c20014580829eb30168d7fac555a9f657ac74142
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-26 11:23:18 +0600

    add pagination

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -12,14 +12,24 @@ const Shop = () => {
     // products to be rendered on the UI
     const [displayProducts, setDisplayProducts] = useState([]);
 
+    // pagination state
+    const [page, setPage] = useState(0);
+    const [pageCount, setPageCount] = useState(0);
+    const size = 10;
+
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+
+        fetch(`http://localhost:5000/products?page=${page}&&size=${size}`)
             .then(res => res.json())
             .then(data => {
-                setProducts(data);
-                setDisplayProducts(data);
+                setProducts(data.products);
+                setDisplayProducts(data.products);
+                const count = data.count;
+                const pageNumber = Math.ceil(count / size);
+                setPageCount(pageNumber);
+
             });
-    }, []);
+    }, [page]);
 
 
 
@@ -67,6 +77,20 @@ const Shop = () => {
                         >
                         </Product>)
                     }
+
+                    <div className='pagination'>
+                        {
+                            [...Array(pageCount).keys()]
+                                .map(number =>
+                                    <button
+                                        className={number === page ? 'selected' : ''}
+                                        key={number}
+                                        onClick={() => setPage(number)}>
+                                        {number + 1}
+                                    </button>)
+                        }
+
+                    </div>
                 </div>
                 <div className="cart-container">
                     <Cart cart={cart}>
